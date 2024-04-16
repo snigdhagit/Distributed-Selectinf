1ead71718ba16cf7d4a51cb2467c09d6e17d396b
liusf15 <sfliu@stanford.edu>
2024-04-15 21:02:43 -0700

    backup

--- Distributed/selectinf/simulations/one_pass.py
+++ Distributed/selectinf/simulations/one_pass.py
@@ -4,7 +4,7 @@ from scipy.stats import norm
 from collections import namedtuple
 import os
 from ..distributed_lasso import multisplit_lasso as L
-from ..Tests.instance import gaussian_instance, logistic_instance
+from ..Tests.instance import gaussian_instance, logistic_instance, gaussian_grouped_instance
 from .metrics import get_metrics
 import argparse
 import pickle
@@ -13,6 +13,12 @@ import itertools
 import regreg.api as rr
 from ..Utils.base import restricted_estimator
 from sklearn.linear_model import LogisticRegression
+
+# filename = "Distributed/selectinf/simulations/results/one_pass_without_replace_K_3_n0_2000_n1_4000_p_100_s_5_signal_0.1_ntune_1000_nsim_500.pkl"
+# with open(filename, 'rb') as f:
+#     res = pickle.load(f)
+
+# exit()
 
 def solve_target_restricted(linpred, X, active):
 
@@ -27,6 +33,25 @@ def solve_target_restricted(linpred, X, active):
     _beta_unpenalized = restricted_estimator(loglike,
                                              active)
     return _beta_unpenalized
+
+def aggregate_shavegroups(_overall):
+
+    group_size = 5
+    ngroup = 20
+
+    groups = np.arange(ngroup).repeat(group_size)
+
+    overall = _overall > 0
+    new_overall = np.zeros(overall.shape[0], np.bool)
+    for g in sorted(np.unique(groups)):
+
+        srt = group_size * g
+        stp = group_size * (g + 1)
+        if overall[srt:stp].sum() > 0:
+            rep = np.random.choice(group_size, 1, replace=False)
+            new_overall[rep + srt] = True
+
+    return new_overall
 
 def run(seedn,
         n=1000,
@@ -43,7 +68,8 @@ def run(seedn,
         sample_with_replacement=False,
         targets='selected',
         n_tuning=0,
-        thre_agg=0):
+        thre_agg=0,
+        one_per_group=False):
     np.random.seed(seedn)
     
     coverages = {}  # cover the target beta in selected/saturated view
@@ -53,14 +79,10 @@ def run(seedn,
     signal = np.sqrt(signal_fac * 2 * np.log(p))
     if not logistic:
         inst = gaussian_instance
-        X, Y, beta = inst(n=n+n_tuning,
-                            p=p,
-                            signal=signal,
-                            s=s,
-                            equicorrelated=False,
-                            rho=rho,
-                            sigma=sigma,
-                            random_signs=True)[:3]
+        if not one_per_group:
+            X, Y, beta = inst(n=n+n_tuning, p=p, signal=signal, s=s, equicorrelated=False, rho=rho, sigma=sigma, random_signs=True)[:3]
+        else:
+            X, Y, beta= gaussian_grouped_instance(n=n+n_tuning, p=p, s=s, sigma=sigma, rho=rho, signal=signal, random_signs=True, equicorrelated=True, n_group=20, one_per_group=one_per_group)[:3]
         if n_tuning > 0:
             X_tune = X[n:]  # used for tuning lambda
             Y_tune = Y[n:]
@@ -68,7 +90,7 @@ def run(seedn,
             Y = Y[:n]
         sigma_ = np.std(Y)
         if n_tuning > 0:
-            weight_facs = np.linspace(.5, 2, 20) 
+            weight_facs = np.linspace(.5, 2, 50) 
             min_mse = np.Inf
             best_weight_fac = .5
             for weight_fac in weight_facs:
@@ -80,7 +102,11 @@ def run(seedn,
                                         estimate_dispersion=True,
                                         sample_with_replacement=sample_with_replacement)
                 
-                signs_ = selector_.fit(thre_agg=thre_agg)
+                if one_per_group:
+                    aggregate=None
+                else:
+                    aggregate = aggregate_shavegroups
+                signs_ = selector_.fit(aggregate=aggregate)
                 mse = np.linalg.norm(Y_tune - X_tune @ selector_._beta_full)**2
                 if mse < min_mse:
                     min_mse = mse
@@ -108,7 +134,7 @@ def run(seedn,
         feature_weights = {i: np.ones(X.shape[1]) * np.sqrt(2 * np.log(p)) for i in range(nK - 1)}
         selector = L.logistic(X, Y, feature_weights, proportion)
         if n_tuning > 0:
-            weight_facs = np.linspace(0.1, 1, num=20)
+            weight_facs = np.linspace(0.1, 1, num=50)
             max_loglik = -np.Inf
             best_weight_fac = weight_facs[0]
             for weight_fac in weight_facs:
@@ -159,8 +185,9 @@ def run(seedn,
         coverages['dist_carving'] = coverage
         lengths['dist_carving'] = length
 
-        accept = (intervals[:, 0] < 0) * (intervals[:, 1] > 0)
-        metrics['dist_carving'] = get_metrics(beta[nonzero], accept)
+        rejects = np.zeros_like(beta, dtype=bool)
+        rejects[nonzero] = (intervals[:, 1] < 0) + (intervals[:, 0] > 0)
+        metrics['dist_carving'] = get_metrics(beta, rejects)
 
         # splitting
         holdout_idx = np.sum(selector._selection_idx, 1) == 0
@@ -184,8 +211,10 @@ def run(seedn,
         coverages['splitting'] = abs(splitting_estimator - beta_target) < sds * norm.ppf(.5 + level / 2)
         lengths['splitting'] = sds * 2 * norm.ppf(.5 + level / 2)
 
-        accept = abs(splitting_estimator) < sds * norm.ppf(.5 + level / 2)
-        metrics['splitting'] = get_metrics(beta[nonzero], accept)
+        # accept = abs(splitting_estimator) < sds * norm.ppf(.5 + level / 2)
+        rejects = np.zeros_like(beta, dtype=bool)
+        rejects[nonzero] = abs(splitting_estimator) > sds * norm.ppf(.5 + level / 2)
+        metrics['splitting'] = get_metrics(beta, rejects)
 
         # naive intervals
         if not logistic:
@@ -205,13 +234,15 @@ def run(seedn,
         coverages['naive'] = abs(beta_ols - beta_target) < full_sds * norm.ppf(.5 + level / 2)
         lengths['naive'] = full_sds * 2 * norm.ppf(.5 + level / 2)
 
-        accept = abs(beta_ols) < full_sds * norm.ppf(.5 + level / 2)
-        metrics['naive'] = get_metrics(beta[nonzero], accept)
+        # accept = abs(beta_ols) < full_sds * norm.ppf(.5 + level / 2)
+        rejects = np.zeros_like(beta, dtype=bool)
+        rejects[nonzero] = abs(beta_ols) > full_sds * norm.ppf(.5 + level / 2)
+        metrics['naive'] = get_metrics(beta, rejects)
 
         return coverages, lengths, metrics, screening
 
 
-def main(seed, sample_with_replacement, nK, n0, n1, p, s, signal_fac, weight_fac, logistic, n_tuning=0, root_dir='', thre_agg=0):
+def main(seed, sample_with_replacement, nK, n0, n1, p, s, signal_fac, weight_fac, logistic, n_tuning=0, root_dir='', thre_agg=0, one_per_group=False):
     print(f"Starting simulation with K={nK}, n0={n0}, n1={n1}, p={p}, s={s}, signal_fac={signal_fac}, thre_agg={thre_agg}")
     nsim = 1
     print_every = 1
@@ -232,7 +263,7 @@ def main(seed, sample_with_replacement, nK, n0, n1, p, s, signal_fac, weight_fac
         # print(n, p, proportion)
 
     for i in range(1):
-        coverages, lengths, metrics, screening = run(seedn=seed, n=n, p=p, nK=nK, sigma=1., signal_fac=signal_fac, rho=0.9, s=s, proportion=proportion, sample_with_replacement=sample_with_replacement, weight_fac=weight_fac, logistic=logistic, n_tuning=n_tuning, thre_agg=thre_agg)
+        coverages, lengths, metrics, screening = run(seedn=seed, n=n, p=p, nK=nK, sigma=1., signal_fac=signal_fac, rho=0.9, s=s, proportion=proportion, sample_with_replacement=sample_with_replacement, weight_fac=weight_fac, logistic=logistic, n_tuning=n_tuning, thre_agg=thre_agg, one_per_group=one_per_group)
         if coverages is None:
             return
 
@@ -251,25 +282,32 @@ def main(seed, sample_with_replacement, nK, n0, n1, p, s, signal_fac, weight_fac
         df_cover = pd.DataFrame(coverages).mean()
         df_len = pd.DataFrame(lengths).mean()
         # print(df_cover)
-        df_metrics = pd.DataFrame(metrics, index=['precision', 'recall', 'f1'])
-        df = pd.concat([df_cover, df_len, df_metrics.T], 1)
+        df_metrics = pd.DataFrame(metrics, index=['TP', 'TN', 'FP', 'FN', 'F1', 'DOR'])
+        df = pd.concat([df_cover, df_len], 1)
+        df.columns = ['coverage', 'length']
+        df = pd.concat([df, df_metrics.T], 1)
         df['num_selected'] = len(coverages['naive'])
         df['screening'] = screening
         print(df)
         
         if root_dir == '':
-            root_dir = 'Distributed/selectinf/simulations/results2'
+            root_dir = 'Distributed/selectinf/simulations/results'
         os.makedirs(root_dir, exist_ok=True)
-        if n_tuning > 0:
-            filename = f"K_{nK}_n0_{n0}_n1_{n1}_p_{p}_s_{s}_signal_{signal_fac}_ntune_{n_tuning}_thre_{thre_agg}_seed_{seed}"
-        else:
-            filename = f"K_{nK}_n0_{n0}_n1_{n1}_p_{p}_s_{s}_signal_{signal_fac}_weight_{weight_fac}_thre_{thre_agg}_seed_{seed}"
+        # if n_tuning > 0:
+        #     filename = f"K_{nK}_n0_{n0}_n1_{n1}_p_{p}_s_{s}_signal_{signal_fac}_ntune_{n_tuning}_thre_{thre_agg}_seed_{seed}"
+        # else:
+        #     filename = f"K_{nK}_n0_{n0}_n1_{n1}_p_{p}_s_{s}_signal_{signal_fac}_weight_{weight_fac}_thre_{thre_agg}_seed_{seed}"
+        filename = f"K_{nK}_n0_{n0}_n1_{n1}_p_{p}_s_{s}_signal_{signal_fac}_ntune_{n_tuning}_seed_{seed}"
+        if one_per_group:
+            filename = f"K_{nK}_n0_{n0}_n1_{n1}_p_{p}_s_{s}_signal_{signal_fac}_ntune_{n_tuning}_1pergroup_seed_{seed}"
+
         if logistic:
             filename += '_logistic'
-        if sample_with_replacement:
-            filename = os.path.join(root_dir, f'one_pass_with_replace_{filename}.csv')
-        else:
-            filename = os.path.join(root_dir, f'one_pass_without_replace_{filename}.csv')
+        # if sample_with_replacement:
+        #     filename = os.path.join(root_dir, f'one_pass_with_replace_{filename}.csv')
+        # else:
+        #     filename = os.path.join(root_dir, f'one_pass_without_replace_{filename}.csv')
+        filename = os.path.join(root_dir, filename + '.csv')
         df.to_csv(filename)
         print("Saved to", filename)
         # all_results = {'coverages': coverages_, 'lengths': lengths_, 'metrics': metrics_, 'screening': screening_}
@@ -284,27 +322,41 @@ if __name__ == "__main__":
     parser.add_argument('--K', default=3, type=int)
     parser.add_argument('--root_dir', default='', type=str)
     parser.add_argument('--logistic', default=False, action='store_true')
+    parser.add_argument('--vary', default='K', type=str)
     args = parser.parse_args()
 
     p = 100
     s = 5
     n0 = 1000
-    # n0_list = [2000, 1000, 500, 250]
-    # n1 = 2000
+    
     weight_fac = 1
-    # main(args.w_replace, K, n0, n1, p, s=5, signal_fac=.5, weight_fac=2.)
-
     K_list = [3, 5, 7, 9]
     nK = args.K
-    # signals = np.linspace(0.5, 2, num=6)
-    # signals = np.linspace(0.1, 0.9, 5)
-    signal_fac = 1.
-
-    # n1 = 8000 // (nK - 1)
-    n1 = 2000
-    for thre_agg in range((nK - 1) // 2):
-        main(args.seed, args.w_replace, nK, n0, n1, p, s, signal_fac, weight_fac, args.logistic, n_tuning=2000, root_dir=args.root_dir, thre_agg=thre_agg)
+    if args.vary == 'K':
+        signal_fac = 0.1
+        n0 = 250
+        for nK in K_list:
+            # n1 = 8000 // (nK - 1)
+            n1 = 2000
+            print('K =', nK)
+            main(args.seed, args.w_replace, nK, n0, n1, p, s, signal_fac, weight_fac, args.logistic, n_tuning=2000, root_dir=args.root_dir, one_per_group=False)
     
+    elif args.vary == 'n0':
+        n0_list = [2000, 1000, 500, 250]
+        signal_fac = .5
+        n1 = 2000
+        nK = 3
+        for n0 in n0_list:
+            main(args.seed, args.w_replace, nK, n0, n1, p, s, signal_fac, weight_fac, args.logistic, n_tuning=2000, root_dir=args.root_dir, one_per_group=False)
+    
+    elif args.vary == 'signal':
+        signals = np.linspace(0.3, 0.9, num=4)
+        n1 = 4000
+        n0 = 1000
+        nK = 2
+        for signal_fac in signals:
+            main(args.seed, args.w_replace, nK, n0, n1, p, s, signal_fac, weight_fac, args.logistic, n_tuning=2000, root_dir=args.root_dir, one_per_group=False)
+
     # len(K_list) * len(signals) * len(n0_list) * len(s_list) * len(weight_facs)
 
     # i = 0
